1614aaa0a639e6049cb186e45b4f36d05fccd904
dmaresch <dmaresch@my.wctc.edu>
2020-04-21 09:47:20 -0500

    dirs inited, pushing content

---
+++ AppDevW3L1/AppDevW3L1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppDevW3L1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int weightTxt;
+            int heightTxt;
+            if(!Int32.TryParse(Inp_Weight.Text,out weightTxt))
+            {
+                Label_BMI.Text = "Invalid text in the Weight field.";
+                return;
+            }
+            if(!Int32.TryParse(Inp_Height.Text,out heightTxt))
+            {
+                Label_BMI.Text = "Invalid text in the Height field.";
+                return;
+            }
+            double weight = 703.0 * weightTxt / Math.Pow(heightTxt,2);
+            Label_BMI.Text = $"Your BMI is {weight:F2}.";
+        }
+    }
+}
